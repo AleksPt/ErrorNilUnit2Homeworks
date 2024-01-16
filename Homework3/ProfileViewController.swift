b0a201ccf4a354206fef7f81c9eb706d7ef389d5
Alex Petrenko <alex.pt@ya.ru>
2024-01-16 19:20:44 +0300

    add nameLabel

--- Homework3/ProfileViewController.swift
+++ Homework3/ProfileViewController.swift
@@ -2,16 +2,32 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var name = "Имя Фамилия"
+    
     lazy var avatar = {
         let avatar = UIImageView()
         avatar.image = UIImage(named: "avatar")
         avatar.frame = CGRect(
             x: view.center.x - 50,
-            y: view.frame.height - view.frame.size.height + 200,
+            y: view.frame.height - view.frame.height + 200,
             width: 100,
             height: 100
         )
         return avatar
+    }()
+    
+    lazy var nameLabel = {
+        let label = UILabel()
+        label.text = name
+        label.font = .boldSystemFont(ofSize: 20)
+        label.textAlignment = .center
+        label.frame = CGRect(
+            x: 0,
+            y: avatar.frame.maxY + 14,
+            width: view.frame.width,
+            height: 24
+        )
+        return label
     }()
     
     override func viewDidLoad() {
@@ -21,6 +37,7 @@ class ViewController: UIViewController {
         navigationController?.navigationBar.prefersLargeTitles = true
         
         view.addSubview(avatar)
+        view.addSubview(nameLabel)
     }
 }
 
