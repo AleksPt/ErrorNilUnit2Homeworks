5654fa480ca29dd0933e376bf9861e78264e8283
Alex Petrenko <alex.pt@ya.ru>
2024-01-16 18:51:37 +0300

    add avatar

--- Homework3/ProfileViewController.swift
+++ Homework3/ProfileViewController.swift
@@ -1,10 +1,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    lazy var avatar = {
+        let avatar = UIImageView()
+        avatar.image = UIImage(named: "avatar")
+        avatar.frame = CGRect(
+            x: view.center.x - 50,
+            y: view.frame.height - view.frame.size.height + 200,
+            width: 100,
+            height: 100
+        )
+        return avatar
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "Профиль"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        view.addSubview(avatar)
     }
 }
 
