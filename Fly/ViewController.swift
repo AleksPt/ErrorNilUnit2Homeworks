9cdec35b9dcd5f144df49f30c50f4edec2959b90
Alex Petrenko <alex.pt@ya.ru>
2024-01-30 23:12:11 +0300

    init

---
+++ Fly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fly
+//
+//  Created by Алексей on 30.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
