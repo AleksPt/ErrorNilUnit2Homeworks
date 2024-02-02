1e0e067f6299a51e5446115a64a09ea7fb6067e1
Alex Petrenko <alex.pt@ya.ru>
2024-02-03 00:18:40 +0300

    add model

---
+++ Drink/Constants.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+struct ConstantsFont {
+    static let titleFont = "DINCondensed-Bold"
+    static let descriptionFont = "HelveticaNeue-UltraLight"
+}
+
+struct ConstantsColor {
+    static let titleColor = UIColor(
+        red: 90 / 255,
+        green: 192 / 255,
+        blue: 208 / 255,
+        alpha: 1
+    )
+    
+    static let backgroundViewColor = UIColor(
+        red: 254 / 255,
+        green: 250 / 255,
+        blue: 236 / 255,
+        alpha: 1
+    )
+    
+    static let buttonColor = UIColor(
+        red: 11 / 255,
+        green: 218 / 255,
+        blue: 81 / 255,
+        alpha: 1
+    )
+}
