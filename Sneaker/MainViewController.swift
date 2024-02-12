3fc4a25bbd776060f24057d8d4af59f743de1aed
Alex Petrenko <alex.pt@ya.ru>
2024-02-12 15:44:50 +0300

    create text field

--- Sneaker/MainViewController.swift
+++ Sneaker/MainViewController.swift
@@ -21,8 +21,23 @@ final class MainViewController: UIViewController {
             height: view.frame.height
         )
         $0.backgroundColor = .red
+        $0.addSubview(textField)
         return $0
     }(UIView())
+    
+    private lazy var textField: UITextField = {
+        $0.frame = CGRect(
+            x: 60,
+            y: view.frame.height - 100,
+            width: view.frame.width - 120,
+            height: 50
+        )
+        $0.backgroundColor = .white
+        $0.placeholder = "Add any text"
+        $0.padding(size: 16)
+        $0.layer.cornerRadius = 10
+        return $0
+    }(UITextField())
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,4 +47,3 @@ final class MainViewController: UIViewController {
 
 
 }
-
